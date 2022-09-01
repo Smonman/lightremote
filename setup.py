e856d68ec4ab8d5d8dda63f5ab53e03e6673b4c0
smonm <e12021358@student.tuwien.ac.at>
2022-09-02 00:11:25 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='lightremote',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='Simon Josef Kreuzpointner',
+    author_email='simonkreuzpointner@gmail.com',
+    description=''
+)
