3bf4b9fc41b8ef751f82812e7f9940aa5f65f575
smonm <e12021358@student.tuwien.ac.at>
2022-09-03 13:30:56 +0200

    add scripts and install_requires to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='lightremote',
-    version='1.0',
-    packages=[''],
-    url='',
-    license='MIT',
-    author='Simon Josef Kreuzpointner',
-    author_email='simonkreuzpointner@gmail.com',
-    description='A primitive remote control for your computer that operates through changes of brighness'
+    name="lightremote",
+    version="1.0.0",
+    packages=[""],
+    url="https://github.com/Smonman/lightremote",
+    license="MIT",
+    author="Simon Josef Kreuzpointner",
+    author_email="simonkreuzpointner@gmail.com",
+    description="A simple remote control that operates through changes of brightness",
+    install_requires=["opencv-python~=4.6.0.66", "mouse~=0.7.1"],
+    scripts=["lightremote.py"]
 )
