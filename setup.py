d09990f761688f5eed52975dff0a09569e3a1d45
smonm <e12021358@student.tuwien.ac.at>
2022-09-05 01:00:51 +0200

    remove packages attribute

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ def readfile(filename):
 setup(
     name="lightremote",
     version="1.0.0",
-    packages=[""],
     url="https://github.com/Smonman/lightremote",
     license=readfile("LICENSE"),
     author="Simon Josef Kreuzpointner",
